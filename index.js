5b51bbef6576ae49f6542e97f430acc3035ee151
aveek-1234 <70470675+aveek-1234@users.noreply.github.com>
2020-10-18 11:37:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const io= require('socket.io')(8000)
+const users ={};
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+        console.log("New user ",name)
+        users[socket.id]=name;
+        socket.broadcast.emit('User-Joined',name)
+    })
+    socket.on('send',message=>{
+        socket.broadcast.emit('recieve',{message:message,name:users[socket.id]})
+    });
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+})
---
+++ js/client.js
@@ -0,0 +1,33 @@
+
+const socket = io('http://localhost:8000');
+const form = document.getElementById('send-container');
+const messageInput=document.getElementById('messageInp');
+
+const messageContainer = document.querySelector(".container");
+
+const append =(message,position)=>{
+   const messageElement= document.createElement('div');
+   messageElement.innerText=message;
+   messageElement.classList.add('message');
+   messageElement.classList.add(position);
+   messageContainer.append(messageElement);
+   
+}
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  const message= messageInput.value;
+  append(message,'right');
+  socket.emit('send',message);
+  messageInput.value='';
+})
+const name=prompt("Enter Your Name");
+socket.emit('new-user-joined',name);
+socket.on('User-Joined',name=>{
+  append( name+' joined','right');
+})
+socket.on('recieve',data=>{
+  append( data.name+'   :'+data.message,'left');
+})
+socket.on('left',name=>{
+  append( name+' left the chat','left');
+})
